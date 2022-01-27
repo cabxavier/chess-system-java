5d0d2defc1c079600886d9412845aef578136ab2
cabxavier <cesaraugustobarbosaxavier@gmail.com>
2022-01-26 23:30:23 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -51,6 +51,24 @@ public class Board {
 		this.pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece removePiece(Position position) {
+		
+		if (!this.positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		
+		if(this.piece(position)==null) {
+			return null;
+		}
+		
+		Piece aux = piece(position);
+		aux.position = null;
+		
+		pieces[position.getRow()][position.getColumn()] = null;
+		
+		return aux;
+	}
 
 	private boolean positionExists(int row, int column) {
 		return ((row >= 0) && (row < this.rows)) && ((column >= 0) && (column < this.columns));
